63380f860d741be858d5ccba9fdc0b171d93819b
Morozov0208Saveliy <savelmor@yandex.ru>
2023-12-03 12:56:11 +0100

    #7.1.1 Исправление

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.google.code.gson:gson:2.10.1")
 
-    implementation ("androidx.viewpager2:viewpager2:1.0.0")
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("androidx.room:room-runtime:2.6.0")
