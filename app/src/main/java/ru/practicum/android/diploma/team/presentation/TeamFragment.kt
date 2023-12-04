1ab3ce746e1e9c5df9fb7cf70d1d55ffb0f62236
kotzimaru <kotzimaru@yandex.ru>
2023-12-04 20:15:36 +0300

    team/01_create_view исправил мелочи

--- app/src/main/java/ru/practicum/android/diploma/team/presentation/TeamFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/team/presentation/TeamFragment.kt
@@ -1,15 +1,6 @@
 package ru.practicum.android.diploma.team.presentation
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.View
 import ru.practicum.android.diploma.R
 
-class TeamFragment : Fragment(R.layout.fragment_team) {
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-
-    }
-}
+class TeamFragment : Fragment(R.layout.fragment_team)
