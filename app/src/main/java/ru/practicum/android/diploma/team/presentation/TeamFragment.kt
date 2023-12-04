89ec186bb656d65cfd331f99750d7801cea443c1
kotzimaru <kotzimaru@yandex.ru>
2023-12-04 16:32:53 +0300

    team/01_create_view исправил импорты

--- app/src/main/java/ru/practicum/android/diploma/team/presentation/TeamFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/team/presentation/TeamFragment.kt
@@ -2,9 +2,7 @@ package ru.practicum.android.diploma.team.presentation
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import ru.practicum.android.diploma.R
 
 class TeamFragment : Fragment(R.layout.fragment_team) {
