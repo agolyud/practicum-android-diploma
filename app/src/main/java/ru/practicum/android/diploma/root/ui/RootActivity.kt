745664f988e9429165eca862650157d0847033ed
Maxim Berezin <beresin.m@googlemail.com>
2023-12-05 22:45:17 +0300

    добавил bottom navigation view

--- app/src/main/java/ru/practicum/android/diploma/root/ui/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/root/ui/RootActivity.kt
@@ -2,8 +2,11 @@ package ru.practicum.android.diploma.root.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 import ru.practicum.android.diploma.BuildConfig
+import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.ActivityRootBinding
+import androidx.navigation.ui.setupWithNavController
 
 class RootActivity : AppCompatActivity() {
 
@@ -15,6 +18,17 @@ class RootActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.container_view) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+
+            }
+        }
 
         // Пример использования access token для HeadHunter API
         networkRequestExample(accessToken = BuildConfig.HH_ACCESS_TOKEN)
