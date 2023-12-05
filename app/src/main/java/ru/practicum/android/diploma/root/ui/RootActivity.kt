d5170a93a71addba9d5c093a77b99266232bab16
Maxim Berezin <beresin.m@googlemail.com>
2023-12-05 22:17:26 +0300

    настройка navigation graph - fix 1

--- app/src/main/java/ru/practicum/android/diploma/root/ui/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/root/ui/RootActivity.kt
@@ -7,12 +7,13 @@ import ru.practicum.android.diploma.databinding.ActivityRootBinding
 
 class RootActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityRootBinding
+    private val binding: ActivityRootBinding by lazy {
+        ActivityRootBinding.inflate(layoutInflater)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityRootBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         // Пример использования access token для HeadHunter API
