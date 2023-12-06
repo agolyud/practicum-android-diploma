8749d2dc3c075a9c1d10b5e208c44ed894f2f669
Maxim Berezin <beresin.m@googlemail.com>
2023-12-06 13:35:40 +0300

    setup bottom navigation menu

---
+++ app/src/main/java/ru/practicum/android/diploma/favorite/presentation/FavoriteFragment.kt
@@ -0,0 +1,6 @@
+package ru.practicum.android.diploma.favorite.presentation
+
+import androidx.fragment.app.Fragment
+import ru.practicum.android.diploma.R
+
+class FavoriteFragment : Fragment(R.layout.fragment_favorite)
