57d3d467e57f1a31be7512328db176e946139612
Maxim Berezin <beresin.m@googlemail.com>
2023-12-09 12:24:07 +0300

    add HH-User-Agent for requests and add Header to getVacancyList

--- app/src/main/java/ru/practicum/android/diploma/util/network/HhApiVacancy.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/network/HhApiVacancy.kt
@@ -1,6 +1,7 @@
 package ru.practicum.android.diploma.util.network
 
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.QueryMap
 import ru.practicum.android.diploma.BuildConfig
 import ru.practicum.android.diploma.search.data.models.SearchResponse
@@ -8,7 +9,10 @@ import ru.practicum.android.diploma.search.data.models.SearchResponse
 
 interface HhApiVacancy {
 
-
+    @Headers(
+        HEADER_AUTH,
+        HH_USER
+    )
     @GET("vacancies")
      fun getVacancyList(
         @QueryMap options: HashMap<String, String>
@@ -18,5 +22,6 @@ interface HhApiVacancy {
 
     companion object{
         const val HEADER_AUTH = "Authorization: Bearer ${BuildConfig.HH_ACCESS_TOKEN}"
+        const val HH_USER = "HH-User-Agent: EmployMe (asa-tek68@mail.ru)"
     }
 }
