d281725d0fb9d04cc7e5c94b11ebf656f0e5a68e
Maxim Berezin <beresin.m@googlemail.com>
2023-12-13 19:30:32 +0300

    - добавил скелет ViewModel - добавил базовый функционал Fragmentа

--- app/src/main/java/ru/practicum/android/diploma/favorite/presentation/FavoriteFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorite/presentation/FavoriteFragment.kt
@@ -1,6 +1,29 @@
 package ru.practicum.android.diploma.favorite.presentation
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import ru.practicum.android.diploma.R
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import ru.practicum.android.diploma.databinding.FragmentFavoriteBinding
 
-class FavoriteFragment : Fragment(R.layout.fragment_favorite)
+class FavoriteFragment : Fragment() {
+    private lateinit var binding: FragmentFavoriteBinding
+    private val viewModel: FavoriteViewModel by viewModel()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentFavoriteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
---
+++ app/src/main/java/ru/practicum/android/diploma/favorite/presentation/FavoriteViewModel.kt
@@ -0,0 +1,6 @@
+package ru.practicum.android.diploma.favorite.presentation
+
+import androidx.lifecycle.ViewModel
+
+class FavoriteViewModel() : ViewModel() {
+}
