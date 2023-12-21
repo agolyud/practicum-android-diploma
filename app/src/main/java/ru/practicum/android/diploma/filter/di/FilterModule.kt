24f8a6da5551fa2ba68b356888d9322c0be8036f
Aleksey Golyudov <alexgolyd@yandex.ru>
2023-12-21 13:52:46 +0300

    Version 1.0

--- app/src/main/java/ru/practicum/android/diploma/filter/di/FilterModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/di/FilterModule.kt
@@ -1,11 +1,19 @@
 package ru.practicum.android.diploma.filter.di
 
 import org.koin.dsl.module
+import ru.practicum.android.diploma.detail.domain.SimilarInteractor
+import ru.practicum.android.diploma.detail.domain.impl.SimilarInteractorImpl
 import ru.practicum.android.diploma.filter.data.FilterRepositoryImpl
 import ru.practicum.android.diploma.filter.domain.api.FilterRepository
+import ru.practicum.android.diploma.filter.domain.impl.FilterInteractor
+import ru.practicum.android.diploma.filter.domain.impl.FilterInteractorImpl
 
 val filterModule = module {
     single<FilterRepository> {
         FilterRepositoryImpl(get(), get())
+    }
+
+    factory<FilterInteractor> {
+        FilterInteractorImpl(repository = get())
     }
 }
