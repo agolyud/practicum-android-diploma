dd6616f0f89152b9486ba6f69db4beae3d6dd127
Maxim Berezin <beresin.m@googlemail.com>
2024-01-04 22:25:24 +0300

    fix: поправил баги кнопки очистки ЗП

--- app/src/main/java/ru/practicum/android/diploma/filter/presentation/fragment/FilterFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/presentation/fragment/FilterFragment.kt
@@ -3,18 +3,27 @@ package ru.practicum.android.diploma.filter.presentation.fragment
 import android.annotation.SuppressLint
 import android.os.Build
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.textfield.TextInputLayout
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentFilterBinding
 import ru.practicum.android.diploma.filter.presentation.states.FilterStates
 import ru.practicum.android.diploma.filter.presentation.viewmodel.FilterViewModel
+import ru.practicum.android.diploma.search.presentation.SearchFragment
 
 class FilterFragment : Fragment(R.layout.fragment_filter) {
     private lateinit var binding: FragmentFilterBinding
@@ -187,6 +196,31 @@ class FilterFragment : Fragment(R.layout.fragment_filter) {
 
         binding.salaryEditText.doOnTextChanged { _, _, _, _ ->
             setVisibilityForChooseBtn()
+        }
+
+        binding.salaryEditText.addTextChangedListener(tWCreator())
+
+        binding.clearIcon.setOnClickListener {
+            binding.clearIcon.visibility = GONE
+            binding.salaryEditText.setText("")
+        }
+    }
+
+    private fun tWCreator() = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            if (!binding.salaryEditText.text.toString().isNullOrEmpty()) {
+                binding.clearIcon.visibility = VISIBLE
+            } else {
+                binding.clearIcon.visibility = GONE
+            }
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+
         }
     }
 
